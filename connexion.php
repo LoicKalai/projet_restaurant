369803967785bbd41500eefd4d5bd511e9d168df
LoicKalai <loic.kalaidjoglou@icloud.com>
2022-03-31 17:42:00 +0200

    [conn]
    connexion

---
+++ connexion.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$db_name = 'Restaurant';
+$username = 'phpmyadmin';
+$password = 'molobo26+';
+
+$conn = new PDO('mysql:host=localhost;dbname=Restaurant', 'phpmyadmin', 'molobo26+');
+
+?>
