d1bbd76476a394f81460c1bf75ddccdd9a35cbf4
Loïc Kalai <97895865+LoicKalai@users.noreply.github.com>
2022-04-05 16:37:49 +0200

    Delete back-office directory
    fichier obsolète erreur de merge

--- back-office/ajout.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$updateprod = $conn->prepare('INSERT INTO produits (Noms, Description, Prix, Images) VALUES (?, ?, ?, ?, ?)');     
-$updateprod->execute(array($nom, $_FILES['imgBO']['name'], $prix, $source));
-?>
